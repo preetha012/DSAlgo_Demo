588c3e9c20f15576a49d78728361b0d5799dd809
preetha012 <preetha012@gmail.com>
2023-07-14 23:19:34 -0400

    Testrunner commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
 
 @CucumberOptions(
 		plugin = {"pretty", "html:target/DSAlgoCucumber.html",
-				//"json:target/cucumber.json",
+				"json:target/cucumber.json",
 				//"junit:target/cucumber-reports/Cucumber.xml",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
